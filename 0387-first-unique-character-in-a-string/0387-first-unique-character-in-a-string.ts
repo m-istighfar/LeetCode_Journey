d5b7e1ee5707e9d519dd63df021ae12945276bca
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-18 23:53:34 +0700

    Time: 95 ms (41.86%), Space: 54.5 MB (42.40%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.ts
@@ -0,0 +1,17 @@
+function firstUniqChar(s: string): number {
+
+    let map = {} 
+
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] = map[s[i]] + 1 || 1;
+    }   
+
+     for (let i = 0; i < s.length; i++) {
+      if (  map[s[i]] == 1) {
+          return i
+      }
+    }  
+
+    return -1 
+    
+};
