49478ab5b7949b58b9e68b6ec1448ab8b23b5418
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-18 23:54:54 +0700

    Time: 62 ms (42.57%), Space: 51.3 MB (18.45%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  let count = 0;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    let left = false,
+      right = false;
+
+    if (flowerbed[i] == 0) {
+      left = i == 0 || flowerbed[i - 1] == 0;
+      right = flowerbed.length - 1 == i || flowerbed[i + 1] == 0;
+    }
+
+    if (left && right) {
+      count++;
+      flowerbed[i] = 1;
+    }
+  }
+
+  return count >= n;
+};
