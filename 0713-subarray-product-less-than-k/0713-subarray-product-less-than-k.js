e24f917e8ae89e0b52cdf82502d34c6ffc81ad50
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-18 23:55:34 +0700

    Time: 63 ms (85.58%), Space: 55.1 MB (10.74%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (k <= 1) return 0;
+    let left = 0, ans = 0, curr = 1;
+    
+    for (let right = 0; right < nums.length; right++) {
+        curr *= nums[right];
+        
+        while (curr >= k) {
+            curr /= nums[left];
+            left++;
+        }
+        
+        ans += right - left + 1;
+    }
+    
+    return ans;
+};
