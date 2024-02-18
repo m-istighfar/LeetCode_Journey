2ca30b1baa84e8ea4ccdd75f0229f5dba35404cb
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-19 00:01:45 +0700

    Time: 47 ms (83.50%), Space: 49.2 MB (5.19%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+
+    const mappedArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        mappedArray.push(fn(arr[i], i));
+    }
+    return mappedArray;
+
+    
+};
