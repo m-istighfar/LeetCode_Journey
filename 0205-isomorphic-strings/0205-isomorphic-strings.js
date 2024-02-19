506aee13bed88d202b1c00af0f3bbc0893f0ed3b
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-19 11:58:55 +0700

    Time: 57 ms (81.94%), Space: 52.3 MB (13.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -3,34 +3,20 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function (s, t) {
-  let sMap = {};
-  let tMap = {};
-
-  for (let i = 0; i < s.length; i++) {
-    let tChar = t[i];
-    let sChar = s[i];
-
-    if (sMap[tChar] !== undefined) {
-      if (sMap[tChar] !== sChar) {
-        return false;
-      }
-    } else {
-      sMap[tChar] = sChar;
+var isIsomorphic = function(s, t) {
+    
+    let map1 = new Map()
+    let map2 = new Map()
+    
+    for (let i = 0; i < s.length; i++) {
+        map1.set(s[i], t[i])
+        map2.set(t[i], s[i])
     }
-
-    if (tMap[sChar] !== undefined) {
-      if (tMap[sChar] !== tChar) {
-        return false;
-      }
-    } else {
-      tMap[sChar] = tChar;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (map1.get(s[i]) !== t[i] || map2.get(t[i]) !== s[i] ) return false;
     }
-  }
-
-  console.log(sMap);
-
-  return true;
+    
+   return true
+    
 };
-
-console.log(isIsomorphic("foo", "bar"));
