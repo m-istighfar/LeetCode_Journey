74feb8068beef9af665ad242db8c4c43f9ad0e90
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-19 18:27:33 +0700

    Time: 65 ms (24.81%), Space: 51.2 MB (22.47%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  let map = new Map();
+  let ans = [];
+
+  let shorter = nums1.length > nums2.length ? nums2 : nums1;
+  let longer = nums1.length > nums2.length ? nums1 : nums2;
+
+  for (const num of longer) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  console.log(map, "map");
+
+  for (let i = 0; i < shorter.length; i++) {
+    if (map.has(shorter[i]) && map.get(shorter[i]) >= 1) {
+      ans.push(shorter[i]);
+      map.set(shorter[i], map.get(shorter[i]) - 1);
+    }
+  }
+
+  return ans;
+};
+
