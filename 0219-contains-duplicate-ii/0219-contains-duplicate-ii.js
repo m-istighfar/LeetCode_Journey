cbff4deef6a54e536097fc962a06d03369102bd6
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-19 23:58:58 +0700

    Time: 89 ms (67.36%), Space: 72.9 MB (19.63%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -1,21 +1,13 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
- */
 var containsNearbyDuplicate = function (nums, k) {
-  let set = new Set();
+  let map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) {
-      return true;
-    }
-    set.add(nums[i]);
-    if (set.size > k) {
-      set.delete(nums[i - k]);
-    }
+    if (map.has(nums[i])) {
+      if (i - map.get(nums[i]) <= k) {
+        return true;
+      } else map.set(nums[i], i);
+    } else map.set(nums[i], i);
   }
 
   return false;
 };
-
