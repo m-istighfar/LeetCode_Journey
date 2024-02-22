e7474ab5553d1915fe286c4ed8f1c3649083abd6
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-23 01:01:07 +0700

    Time: 84 ms (97.38%), Space: 54.8 MB (16.20%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+
+    nums.sort((a,b) => a-b)
+
+    let ans = 0
+
+    for (let i = 0; i < nums.length;i++) {
+        if (i % 2 != 1) {
+            ans += nums[i]
+        }
+    }
+
+    return ans
+    
+};
