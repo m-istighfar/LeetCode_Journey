3b2dfec579d143d74a35a26e0af00e5652bdcb1b
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-24 23:32:54 +0700

    Time: 41 ms (99.05%), Space: 49.5 MB (50.32%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+
+    let ans = BigInt(num1) * BigInt(num2)
+
+    return String(ans)
+    
+};
