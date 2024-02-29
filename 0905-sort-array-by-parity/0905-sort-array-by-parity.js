05439eb6cb72598694f4a70b12339c521a7bb264
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-02-29 22:14:41 +0700

    Time: 64 ms (72.18%), Space: 52.3 MB (31.30%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -7,21 +7,19 @@ var sortArrayByParity = function (nums) {
   let right = nums.length - 1;
   let temp;
 
-  nums.sort((a, b) => a - b);
-
 
   while (left < right) {
-    if ((nums[left] % 2 !== 0) & (nums[right] % 2 == 0)) {
+    if ((nums[left] % 2 !== 0) && (nums[right] % 2 == 0)) {
       temp = nums[left];
       nums[left] = nums[right];
       nums[right] = temp;
       left++;
       right--;
-    } else if ((nums[left] % 2 !== 0) & (nums[right] % 2 !== 0)) {
+    } else if ((nums[left] % 2 !== 0) &&  (nums[right] % 2 !== 0)) {
       right--;
-    } else if ((nums[left] % 2 == 0) & (nums[right] % 2 == 0)) {
+    } else if ((nums[left] % 2 == 0) && (nums[right] % 2 == 0)) {
       left++;
-    } else if ((nums[left] % 2 == 0) & (nums[right] % 2 !== 0)) {
+    } else if ((nums[left] % 2 == 0) && (nums[right] % 2 !== 0)) {
       left++;
       right--;
     }
