af0ffd9b3d09edc1a2b9ca381d978c81ce5a8a14
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-03 18:14:36 +0700

    Time: 245 ms (16.72%), Space: 82.4 MB (35.08%) - LeetHub

---
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function(arr, k) {
+
+    let map = new Map()
+    let ans = 0
+    let check = 0
+
+    for (let nums of arr) {
+        map.set(nums, (map.get(nums) || 0) + 1)
+    }
+
+    sortedMap = new Map([...map.entries()].sort((a,b) => a[1] - b[1]))
+
+    for (const keys of sortedMap.keys()) {
+        if (sortedMap.get(keys) <= k ) {
+            k -= sortedMap.get(keys)
+            sortedMap.delete(keys)
+
+        }
+
+        if (k == 0) break;
+    }
+
+   let set = new Set([...sortedMap.keys()])
+
+   return set.size
+    
+};
