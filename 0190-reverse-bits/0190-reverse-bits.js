d77f37cd41e80ea965d44038ee5b9b7d16cb26ce
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-05 00:05:59 +0700

    Time: 71 ms (10.27%), Space: 50.9 MB (41.17%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    
+    
+    return parseInt(n.toString(2).split("").reverse().join("").padEnd(32, "0"),2)
+};
