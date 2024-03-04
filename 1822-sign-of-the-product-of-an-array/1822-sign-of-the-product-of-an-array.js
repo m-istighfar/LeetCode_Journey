59251317fbfbc0042917510feeb3940bcdbff9a0
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-04 19:36:49 +0700

    Time: 53 ms (58.50%), Space: 50.2 MB (27.32%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function (nums) {
+  const product = nums.reduce((acc, curr) => acc * curr, 1);
+
+  if (product > 0) {
+    return 1;
+  } else if (product < 0) {
+    return -1;
+  } else return 0;
+};
