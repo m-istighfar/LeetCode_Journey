3253f6183f5ce1143d74b3bb205b3293cca4eb19
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-06 02:09:14 +0700

    Time: 2703 ms (42.84%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,8 @@
+SELECT S.name 
+FROM SalesPerson S
+WHERE S.sales_id NOT IN (
+    SELECT O.sales_id 
+    FROM Orders O
+    INNER JOIN Company C ON O.com_id = C.com_id
+    WHERE C.name = "RED"
+)
