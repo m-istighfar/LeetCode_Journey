6014c33a45b8374f54ed3fc9adaff7148d55b875
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-11 23:36:27 +0700

    Time: 55 ms (73.20%), Space: 49.5 MB (86.02%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    
+    let ans = []
+    
+    for (let i = 0; i < s.length; i++) {
+        ans[indices[i]] = s[i]
+    }
+    
+    return ans.join("")
+    
+}; 
