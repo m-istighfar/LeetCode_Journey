effaee069e51a1b282288951c265a4e2549bb81d
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-12 13:08:06 +0700

    Time: 53 ms (90.97%), Space: 51.1 MB (72.03%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    
-    let arr1 = nums.slice(0, n)
-    let arr2 = nums.slice(n, 2*n)
-    
+   
     let ans = []
     
     for (let i = 0; i < n; i++) {
-        ans.push(arr1[i])
-         ans.push(arr2[i])
+        ans.push(nums[i])
+         ans.push(nums[n+i])
         
     }
     
