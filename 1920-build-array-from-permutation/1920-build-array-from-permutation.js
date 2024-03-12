2981730496f1b2c62e0de09a47dcd521cfff3153
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-12 12:39:46 +0700

    Time: 79 ms (8.49%), Space: 53.7 MB (33.76%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    
+    let ans = []
+    
+    for (let i = 0; i < nums.length ; i++) {
+        ans[i] = nums[nums[i]]
+    }
+    
+    return ans
+    
+};
