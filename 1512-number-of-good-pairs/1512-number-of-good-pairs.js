f4cbf7c7576c1f74000c4899212b08b64d4afda6
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-12 12:44:30 +0700

    Time: 44 ms (93.11%), Space: 49 MB (30.69%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    
+    let ans = 0
+    
+    
+    
+     for (let i = 0; i < nums.length-1; i++) {
+         
+         for (let j = i+1; j < nums.length; j++) {
+             if (nums[i] == nums[j]) ans++
+       
+    }
+       
+    }
+    
+    return ans
+    
+    
+    
+};
