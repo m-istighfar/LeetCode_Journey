ad7e32bae5ede16d75e3fa8ec23efeb6e7d1e03a
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-20 05:21:16 +0700

    Time: 82 ms (28.04%), Space: 53.6 MB (45.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,22 +4,11 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  let map = {};
-  let map2 = {};
+    
+    let sArr =  s.split("").sort().join("")
+    let tArr = t.split("").sort().join("")
+    
 
-  if (s.length !== t.length) return false;
-
-  for (let i = 0; i < s.length; i++) {
-    map[s[i]] = map[s[i]] + 1 || 1;
-    map2[t[i]] = map2[t[i]] + 1 || 1;
-  }
-
-  for (const [key] of Object.entries(map)) {
-    console.log(map, map2);
-    if (map[key] != map2[key]) {
-      return false;
-    }
-  }
-
-  return true;
+    
+  return sArr == tArr
 };
