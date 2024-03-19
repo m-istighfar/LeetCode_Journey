8bed27df5ce4a2c7b73e5ebf3dc7397d3083d6d3
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-20 05:18:20 +0700

    Time: 71 ms (72.83%), Space: 59 MB (54.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -11,9 +11,9 @@ var maxProfit = function(prices) {
     let max = 0
 
     for (let i = 0; i < prices.length  ; i++) {
-      profit = prices[i] - min;
-      max = Math.max(max, profit);
+      profit = prices[i] - min
       min = Math.min(min, prices[i])
+      max = Math.max(max, profit)
     }
 
     return max;
