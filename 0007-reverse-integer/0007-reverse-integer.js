9687e59cdaafb2ceec433835bcb0c8d985714a30
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-20 15:11:17 +0700

    Time: 87 ms (11.37%), Space: 52.9 MB (50.14%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,39 +3,29 @@
  * @return {number}
  */
 var reverse = function(x) {
-
-
-    let ans = []
-    let digit = 0
-    let min = false
-
-    if (x < 0) {
-        x = x * -1
-        min = true
-    }
-
-   
-
-    while (x % 10 == 0 && x !== 0) {
-        x = x / 10
-    }
     
-    while ( x > 0 ) {
-      
-            digit = x % 10
-            ans.push(digit)
-            x = Math.floor(x/10)
-        
-    }
-
-    let sum = 0;
-    for (let i = 0; i < ans.length;i++) {
-        sum = sum + ans[i] * (10 **  (ans.length-1-i))
-    }
-
-    if (sum < (-(2 ** 31)) || sum > (2 ** 31) - 1) {
-        return 0
-    }
+ const isNegative = () => {
+     return x < 0
+ }
     
-    return (min == true) ? sum*-1 : sum
+
+ let number = Math.abs(x)
+ 
+
+    
+ let ans = 0
+ 
+ while (number > 0) {
+     let digit = number % 10
+     number = Math.floor(number/10)
+     ans = ans * 10
+     ans += digit
+ }
+
+ if (ans > (Math.pow(2, 31) - 1)) {
+            return 0;
+        }
+    
+    return isNegative() ? -1*ans : ans
+    
 };
