17e4a7cdd96dd404efaf612eb906e6271fcdec51
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-20 23:48:50 +0700

    Time: 51 ms (88.76%), Space: 50.5 MB (62.55%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.js
+++ 0273-integer-to-english-words/0273-integer-to-english-words.js
@@ -2,28 +2,68 @@
  * @param {number} num
  * @return {string}
  */
-var numberToWords = function(num) {
-    if (num === 0) return 'Zero';
+var numberToWords = function (num) {
+  if (num == 0) {
+    return "Zero";
+  }
 
-    const belowTwenty = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 
-                         'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
-    const tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
-    const thousands = ['', 'Thousand', 'Million', 'Billion'];
+  let underTwenty = [
+    "",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Eleven",
+    "Twelve",
+    "Thirteen",
+    "Fourteen",
+    "Fifteen",
+    "Sixteen",
+    "Seventeen",
+    "Eighteen",
+    "Nineteen",
+  ];
+  let tens = [
+    "",
+    "",
+    "Twenty",
+    "Thirty",
+    "Forty",
+    "Fifty",
+    "Sixty",
+    "Seventy",
+    "Eighty",
+    "Ninety",
+  ];
 
-    function toWords(n) {
-        if (n === 0) return '';
-        else if (n < 20) return belowTwenty[n] + ' ';
-        else if (n < 100) return tens[Math.floor(n / 10)] + ' ' + toWords(n % 10);
-        else return belowTwenty[Math.floor(n / 100)] + ' Hundred ' + toWords(n % 100);
+  let thousand = ["", "Thousand", "Million", "Billion"];
+
+  const toWords = (n) => {
+    if (n === 0) return "";
+    else if (n < 20) {
+      return underTwenty[n] + " ";
+    } else if (n < 100) {
+      return tens[Math.floor(n / 10)] + " " + toWords(n % 10);
+    } else
+      return underTwenty[Math.floor(n / 100)] + " Hundred " + toWords(n % 100);
+  };
+
+  let output = "",
+    i = 0;
+
+  while (num > 0) {
+    if (num % 1000 !== 0) {
+      output = toWords(num % 1000) + thousand[i] + " " + output;
     }
+    num = Math.floor(num / 1000);
+    i++;
+  }
 
-    let output = '', i = 0;
-    while (num > 0) {
-        if (num % 1000 !== 0){
-            output = toWords(num % 1000) + thousands[i] + ' ' + output;}
-        num = Math.floor(num / 1000);
-        i++;
-    }
-
-    return output.trim();
+  return output.trim();
 };
