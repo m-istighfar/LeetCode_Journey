dde4e4d8608b775dfca29a6635310d0a70e611fd
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-20 15:22:56 +0700

    Time: 112 ms (62.94%), Space: 58.2 MB (11.16%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -2,38 +2,31 @@
  * @param {number} num
  * @return {string}
  */
-var intToRoman = function(num) {
+var intToRoman = function (num) {
+  let map = new Map([
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ]);
 
-    let map = new Map([
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1]
-    ]);
+  let ans = [];
 
-
-
-
-
-
-let ans = []
-
-    for (let [key, value] of map) {
-        
-        while (num >= value) {
-            ans.push(key)
-            num = num - value
-        }
+  for (let [key, value] of map.entries()) {
+    while (num >= value) {
+      ans.push(key);
+      num -= value;
     }
-     
-    return ans.join("")
+  }
+
+  return ans.join("");
 };
