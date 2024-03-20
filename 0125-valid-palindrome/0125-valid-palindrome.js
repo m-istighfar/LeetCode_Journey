fa4b946a92d45f2221c1da5da83962d95c4ba6a5
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-20 15:58:19 +0700

    Time: 64 ms (52.98%), Space: 54 MB (30.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,28 +1,10 @@
 function isPalindrome(s) {
-  let left = 0,
-    right = s.length - 1;
 
-  function isAlphaNumeric(c) {
-    let code = c.charCodeAt(0);
-    return (
-      (code >= 48 && code <= 57) ||
-      (code >= 65 && code <= 90) ||
-      (code >= 97 && code <= 122)
-    );
-  }
-
-  while (left < right) {
-    while (left < right && !isAlphaNumeric(s[left])) left++;
-
-    while (left < right && !isAlphaNumeric(s[right])) right--;
-
-    if (left < right && s[left].toLowerCase() !== s[right].toLowerCase()) {
-      return false;
-    }
-
-    left++;
-    right--;
-  }
-
-  return true;
+    let filtered = s.toLowerCase().replace(/[^a-z0-9]/g, "")
+    
+    console.log(filtered)
+    
+    let reversedFilter = filtered.split("").reverse().join("")
+    
+    return filtered == reversedFilter
 }
