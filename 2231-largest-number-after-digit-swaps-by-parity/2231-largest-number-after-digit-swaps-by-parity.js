43fdb53f8ac34ce85761385339a3a31bf6d4524c
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-23 22:34:10 +0700

    Time: 69 ms (6.19%), Space: 50.9 MB (23.01%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var largestInteger = function(num) {
+    
+    let arrNum = Array.from(String(num),Number).sort((a,b) => a-b)
+    let odd = arrNum.filter(num => num % 2 == 1 )   
+    let even = arrNum.filter(num => num % 2 == 0 )  
+    console.log(odd)
+    
+    let ans = ""
+    
+    for (let i = 0; i < arrNum.length; i++) {
+        if (String(num)[i] % 2 == 0) {
+            ans += even.pop()
+        } else {
+            ans += odd.pop()
+        }
+    }
+    
+    return Number(ans)
+    
+};
