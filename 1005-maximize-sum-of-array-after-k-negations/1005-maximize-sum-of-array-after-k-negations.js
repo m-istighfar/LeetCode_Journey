6f75724fb0d69ab7d67f98c9a7e3c2e99ce71501
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-24 01:15:24 +0700

    Time: 57 ms (74.16%), Space: 50.3 MB (82.30%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.js
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.js
@@ -4,13 +4,18 @@
  * @return {number}
  */
 var largestSumAfterKNegations = function (nums, k) {
- 
-for (let i = 1; i <= k; i++) {
-    let n = Math.min(...nums)
-    let idx = nums.indexOf(n)
-    nums[idx] *= -1
-}
-    
-return nums.reduce((a,b) => a+b,0)
+  nums.sort((a, b) => Math.abs(b) - Math.abs(a));
 
+  for (let i = 0; i < nums.length && k > 0; i++) {
+    if (nums[i] < 0) {
+      nums[i] *= -1;
+      k--;
+    } 
+  }
+    
+  if (k % 2 == 1) {
+      nums[nums.length-1] *= -1
+  }
+    
+  return nums.reduce((a,b) => a+b,0)
 };
