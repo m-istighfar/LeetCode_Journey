e9f59b7ce293960e597c7a78146982367c20f9e6
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-03-24 01:10:34 +0700

    Time: 83 ms (19.62%), Space: 55.2 MB (18.66%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.js
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.js
@@ -4,44 +4,13 @@
  * @return {number}
  */
 var largestSumAfterKNegations = function (nums, k) {
-  nums.sort((a, b) => {
-    if (a < 0 && b < 0) {
-      return a - b;
-    } else {
-      return b - a;
-    }
-  });
+ 
+for (let i = 1; i <= k; i++) {
+    let n = Math.min(...nums)
+    let idx = nums.indexOf(n)
+    nums[idx] *= -1
+}
+    
+return nums.reduce((a,b) => a+b,0)
 
-
-  let minCount = 0;
-  let getBase = false;
-  let base = nums.length;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0) {
-      minCount++;
-      if (!getBase) {
-        base = i;
-        getBase = true;
-      }
-    }
-  }
-
-  let index = base - 1;
-
-  while (k > 0) {
-    if (minCount > 0) {
-      nums[base] *= -1;
-      minCount--;
-      k--;
-      base++;
-    } else {
-      nums[index] < nums[nums.length - 1]
-        ? ((nums[index] *= -1), k--)
-        : ((nums[nums.length - 1] *= -1), k--);
-    }
-  }
-
-
-  return nums.reduce((a, b) => a + b, 0);
 };
