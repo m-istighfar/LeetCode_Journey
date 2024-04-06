9ce742ed6105008036e500be20bd9560ce8059a9
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-04-06 22:31:51 +0700

    Time: 64 ms (24.13%), Space: 52.9 MB (11.76%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function (nums) {
+  const numStrs = nums.map(String);
+
+  numStrs.sort((a, b) => {
+    const order1 = a + b;
+    const order2 = b + a;
+
+    return order2 - order1;
+  });
+    
+ console.log(numStrs)
+
+  if (numStrs[0] === "0") {
+    return "0";
+  }
+
+  return numStrs.join("");
+};
+
+console.log(largestNumber([3, 30, 34, 5, 9]));
