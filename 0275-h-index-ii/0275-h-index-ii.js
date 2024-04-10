45986634ed02aa1f102e328b7d2ca83d718907a6
M. Istighfar Amal <135976183+m-istighfar@users.noreply.github.com>
2024-04-10 21:36:49 +0700

    Time: 60 ms (23.58%), Space: 50.9 MB (54.72%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    let left = 0, right = citations.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (citations[mid] >= citations.length - mid) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return citations.length - left;
+};
